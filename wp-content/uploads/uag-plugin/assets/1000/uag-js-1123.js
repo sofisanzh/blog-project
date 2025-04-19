3645597fa13b3f4746f02b2d4aa73094203d97a3
sofisanzh <sofisanzh@gmail.com>
2025-04-19 03:11:41 -0400

    Uploading first version of blog site

---
+++ wp-content/uploads/uag-plugin/assets/1000/uag-js-1123.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-6c616363' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-74cc16b3' );
+});
+ });
